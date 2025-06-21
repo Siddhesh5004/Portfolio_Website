d8c2460080ba69f0b1e1d172075a192e6bb510b1
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2025-06-21 23:50:46 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ 
   plugins: [react()],
-  base :"/Portfolio_Website" ,
+  base: './' ,
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
